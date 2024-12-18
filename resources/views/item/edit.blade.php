008d97fc8abe17c7c7125f2e927449a8470033d5
tanshiro0185 <tanshiro0804@gmail.com>
2024-12-18 18:31:50 +0900

    maxlength

--- resources/views/item/edit.blade.php
+++ resources/views/item/edit.blade.php
@@ -36,10 +36,10 @@
                          @endforeach
                         </select>
                         </div>
-                        
+
                         <div class="form-group">
                              <label for="detail">詳細</label>
-                             <textarea class="form-control" id="detail" name="detail" placeholder="詳細説明" rows="4">{{ $item->detail }}</textarea>
+                             <textarea class="form-control" id="detail" name="detail" placeholder="詳細説明" maxlength="2000">{{ $item->detail }}</textarea>
                              </div>
                     </div>
 
